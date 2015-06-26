f36b00f4a2edead9f2d7f9c2fda1c8f91e2a40d3
Paul Kim <paulkim517@gmail.com>
2015-06-26 16:01:07 -0700

    added more dynamic features; made it so that only one page is open at a time; figured out javascript of how to make dynamic features happen

--- scripts.js
+++ scripts.js
@@ -1,8 +1,50 @@
 window.onload=function() {
+	
 	var infoForm = document.getElementById("whatever");
+
+var aboutBtn = document.getElementById("about-btn");
+
+ var projectsBtn = document.getElementById("projects-btn");
+
+ var contactBtn = document.getElementById("contact-btn");
+ 
+
+
+
+
+  aboutBtn.addEventListener("click", function(event) {
+    document.getElementById("about").style.display = "block";
+      document.getElementById("projects").style.display = "none";
+      document.getElementById("contacts").style.display = "none";
+     
+    
+  });
+
+
+  projectsBtn.addEventListener("click", function(event) {
+     console.log("suck it")
+      document.getElementById("projects").style.display = "block";
+     document.getElementById("about").style.display = "none";
+      document.getElementById("contacts").style.display = "none";
+     
+   
+  });
+
+  contactBtn.addEventListener("click", function(event){
+	
+      	document.getElementById("contacts").style.display = "block";
+      	document.getElementById("about").style.display = "none";
+      document.getElementById("projects").style.display = "none";
+      	
+  });
+
 
 	infoForm.addEventListener("click", function() {
 	  event.preventDefault();
 	  alert("Thank's,I'll reach out to you soon!");
 	});
-};
+}
+
+ 
+
+
